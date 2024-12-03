ac6078c1d48d5d701e6e5c4b5abeb5f6c35b8ae2
EC2 Default User <ec2-user@ip-172-31-32-175.eu-west-1.compute.internal>
2024-12-03 13:08:40 +0000

    encoding changes in flask app

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask, request, render_template
 import joblib
 import numpy as np
+import boto3
+import json
+
+# AWS Lambda client
+lambda_client = boto3.client('lambda', region_name='eu-west-1')
+
 
 app = Flask(__name__)
 
@@ -15,17 +21,17 @@ encoding_strategies = {
     'work_interfere': {'Never': 0, 'Rarely': 1, 'Sometimes': 2, 'Often': 3},
     'no_employees': {'1-5': 0, '6-25': 1, '26-100': 2, '100-500': 3, '500-1000': 4, 'More than 1000': 5},
     'tech_company': {'Yes': 1, 'No': 0},
-    'benefits': {'Yes': 1, 'No': 0},
+    'benefits': {'Yes': 1, 'No': 0, "Don't know": 2},
     'care_options': {'Yes': 1, 'No': 0, 'Not sure': 2},
-    'wellness_program': {'Yes': 1, 'No': 0},
-    'seek_help': {'Yes': 1, 'No': 0},
-    'anonymity': {'Yes': 1, 'No': 0},
-    'leave': {'Very easy': 0, 'Somewhat easy': 1, 'Somewhat difficult': 2, 'Very difficult': 3},
-    'mental_health_consequence': {'Yes': 1, 'No': 0},
-    'phys_health_consequence': {'Yes': 1, 'No': 0},
-    'coworkers': {'Yes': 1, 'No': 0},
-    'supervisor': {'Yes': 1, 'No': 0},
-    'mental_vs_physical': {'Yes': 1, 'No': 0},
+    'wellness_program': {'Yes': 1, 'No': 0, "Don't know": 2},
+    'seek_help': {'Yes': 1, 'No': 0, "Don't know": 2},
+    'anonymity': {'Yes': 1, 'No': 0, "Don't know": 2},
+    'leave': {'Very easy': 0, 'Somewhat easy': 1, 'Somewhat difficult': 3, "Don't know": 2},
+    'mental_health_consequence': {'Yes': 1, 'No': 0, 'Maybe': 2},
+    'phys_health_consequence': {'Yes': 1, 'No': 0, 'Maybe': 2},
+    'coworkers': {'Yes': 1, 'No': 0, 'Some of them': 2},
+    'supervisor': {'Yes': 1, 'No': 0, 'Some of them': 2},
+    'mental_vs_physical': {'Yes': 1, 'No': 0, "Don't know": 2},
     'sentiment_encoded': {'Positive': 1, 'Negative': -1, 'Neutral': 0}
 }
 
@@ -39,7 +45,7 @@ def index():
         'wellness_program': ['Yes', 'No', "Don't know"],
         'seek_help': ['Yes', 'No', "Don't know"],
         'anonymity': ['Yes', 'No', "Don't know"],
-        'leave': ['Very easy', 'Somewhat easy', 'Somewhat difficult', 'Very difficult'],
+        'leave': ['Very easy', 'Somewhat easy', 'Somewhat difficult', "Don't know"],
         'mental_health_consequence': ['Yes', 'No', 'Maybe'],
         'phys_health_consequence': ['Yes', 'No', 'Maybe'],
         'coworkers': ['Yes', 'No', 'Some of them'],
@@ -75,6 +81,24 @@ def predict():
     prediction = model.predict(processed_data)
     result = "Needs Treatment" if prediction[0] == 1 else "No Treatment Needed"
 
+     # Prepare data for Lambda
+    lambda_payload = {
+        "prediction_result": result,
+        "user_data": data
+    }
+
+    try:
+        # Invoke Lambda function
+        response = lambda_client.invoke(
+            FunctionName='x23178248_save_prediction_result',
+            InvocationType='Event',  # Use 'Event' for asynchronous invocation
+            Payload=json.dumps(lambda_payload)
+        )
+        print(f"Lambda invocation response: {response}")
+    except Exception as e:
+        print(f"Error invoking Lambda: {e}")
+
+
     # Render the prediction on the same page
     dynamic_fields = {
         'benefits': ['Yes', 'No', "Don't know"],
@@ -82,7 +106,7 @@ def predict():
         'wellness_program': ['Yes', 'No', "Don't know"],
         'seek_help': ['Yes', 'No', "Don't know"],
         'anonymity': ['Yes', 'No', "Don't know"],
-        'leave': ['Very easy', 'Somewhat easy', 'Somewhat difficult', 'Very difficult'],
+        'leave': ['Very easy', 'Somewhat easy', 'Somewhat difficult',  "Don't know"],
         'mental_health_consequence': ['Yes', 'No', 'Maybe'],
         'phys_health_consequence': ['Yes', 'No', 'Maybe'],
         'coworkers': ['Yes', 'No', 'Some of them'],
@@ -93,4 +117,5 @@ def predict():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    #app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
